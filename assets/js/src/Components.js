5226f1fb001a3dae85f291be141543631f5add5b
paulsenm <paulsenm@my.lanecc.edu>
2023-05-30 19:06:11 -0700

    Added icon size options, icon on forecast days.

--- assets/js/src/Components.js
+++ assets/js/src/Components.js
@@ -3,11 +3,56 @@ import {vNode} from "../../../node_modules/@ocdladefense/view/view.js";
 import {getDate, getWeekday} from "../lib-date/src/dates.js";
 import { Modal } from "../../../dev_modules/node-modal/dist/modal.js";
 
-const WeatherDetail = function(props) {
+
+const Forecast = function(props){
+  let forecast = props.forecast;
+  let iconUrl = "http://openweathermap.org/img/wn/";
+  let html = forecast.map((forecastDay, index) => {return <ForecastDay day={forecastDay} index={index} url={iconUrl} size="medium" />;});
+    return (
+      <div class="weather-list flex-parent">
+          {html}
+      </div>
+    )
+  };
+  
+  const ForecastDay = function(props){
     let day = props.day;
+    let index = props.index;
+    let theDate = getDate(day.dt);
+    let url = props.url;
+    let size = props.size
+    let report = day.weather[0]; // The weather report; like you'd hear on the radio.
+
+    let sizeObject = {
+      small: ".png",
+      medium: "@2x.png",
+      large: "@4x.png"
+    };
+    let iconUrl = url + report.icon + sizeObject[size];
+ 
+    return (
+        <div class="weather-list-item" data-action="details" data-index={index}>
+            {(theDate.getMonth() + 1) + "/" + theDate.getDate()}<br />
+            <img src={iconUrl} data-action="details" data-index={index} /> <br />
+            {getWeekday(theDate)}<br />
+            {Math.round(day.temp.min) + " | " + Math.round(day.temp.max)}<br />
+            {/* <button data-action="email" data-index={index} >Email Forecast</button> */}
+        </div>
+    )
+};
+
+const ForecastDayDetail = function(props) {
+    let day = props.day;
+    let url = props.url;
+    let size = props.size
     let report = day.weather[0]; // The weather report; like you'd hear on the radio.
     let temp = day.temp;
-    let iconUrl = `http://openweathermap.org/img/wn/${report.icon}.png`;
+    let sizeObject = {
+      small: ".png",
+      medium: "@2x.png",
+      large: "@4x.png"
+    };
+    let iconUrl = url + report.icon + sizeObject[size];
   
     const foobar = (
         <div class="details">
@@ -24,35 +69,10 @@ const WeatherDetail = function(props) {
     return foobar;
   };
 
-const Forecast = function(props){
-    let forecast = props.forecast;
-    let html = forecast.map((forecastDay, index) => {return <ForecastDay day={forecastDay} index={index} />;});
-    return (
-      <div class="weather-list flex-parent">
-          {html}
-      </div>
-    )
-};
-
-const ForecastDay = function(props){
-    let day = props.day;
-    let index = props.index;
-    let theDate = getDate(day.dt);
- 
-    return (
-        <div class="weather-list-item" data-action="details" data-index={index}>
-            {(theDate.getMonth() + 1) + "/" + theDate.getDate()}<br />
-            {getWeekday(theDate)}<br />
-            {day.temp.min + " | " + day.temp.max}<br />
-            {/* <button data-action="email" data-index={index} >Email Forecast</button> */}
-        </div>
-    )
-};
-
 const EmailDraft = function(props){
   let content = props.content;
   return (
-      <div>
+    <div class="text-center m-2 p-4">
         <form>
           <label for="recipient">Send to: </label>
           <input type="text" id="recipient" name="recipient" />
@@ -73,4 +93,4 @@ const EmailDraft = function(props){
 
 
 
-export {WeatherDetail, Forecast, ForecastDay, EmailDraft};
+export {ForecastDayDetail, Forecast, ForecastDay, EmailDraft};
