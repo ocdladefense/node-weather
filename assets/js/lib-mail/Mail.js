1e548be5002d9600d37f3d0dabda4c0fdc15c06f
Ubuntu <ubuntu@ip-172-31-58-171.us-west-2.compute.internal>
2023-06-01 11:41:52 -0700

    New mailer module.

---
+++ assets/js/lib-mail/Mail.js
@@ -0,0 +1,55 @@
+
+export {Mailer};
+
+
+
+class Mailer {
+
+async  WxMail(recipient, subject, body){
+  let data = {
+    recipient: recipient,
+    subject: subject,
+    body: body
+  };
+  if (this.emailUrl == null) {
+    throw new Error("You have to configure EMAIL_URL in .env");
+  }
+  let response = await fetch(this.emailUrl, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+  let result = await response.text();
+  console.log(result);
+}
+
+async sendWxMail(){
+  let recipient = "mwpaulsen86@gmail.com";
+  let subject = "This is the subject";
+  let body = "this is the email body";
+
+  await this.WxMail(recipient, subject, body);
+}
+
+
+async function wxMail(recipient, subject, body){
+    let data = {
+      recipient: recipient,
+      subject: subject,
+      body: body
+    };
+    let response = await fetch('/email', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    let result = await response.text();
+    console.log(result);
+  }
+
+}
+
