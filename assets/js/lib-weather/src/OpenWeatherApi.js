0c5acc08cd0751ffafa00d61527bf539d86f20b9
Ubuntu <ubuntu@ip-172-31-58-171.us-west-2.compute.internal>
2023-06-01 11:34:48 -0700

    Let user specify number of days to get the forecast for.

--- assets/js/lib-weather/src/OpenWeatherApi.js
+++ assets/js/lib-weather/src/OpenWeatherApi.js
@@ -1,13 +1,35 @@
 import formatQueryString from "../../lib-http/src/http.js";
 
+// For more information,
+//  see https://openweathermap.org/api
+
+const OPEN_WEATHER_API_VERSION = '2.5';
+// const OPEN_WEATHER_API_VERSION = '3.0';
+
+
+// One Call API 3.0
+const OPEN_WEATHER_API_ONE_CALL = "https://api.openweathermap.org/data/" + OPEN_WEATHER_API_VERSION + "/onecall";
+
+// Historical weather data
+// Example: https://api.openweathermap.org/data/3.0/onecall/timemachine?lat={lat}&lon={lon}&dt={time}&appid={apiKey}
+const OPEN_WEATHER_API_HISTORICAL = "https://api.openweathermap.org/data/" +OPEN_WEATHER_API_VERSION+"/onecall/timemachine";
+
+// sample openweathermap api call
+//https://api.openweathermap.org/data/2.5/onecall?lat=43.9698&lon=-123.2006&exclude=minutely,hourly,current&units=imperial&appid=3fca0a11ad63bd24761e381b964b5ae9
+
 class OpenWeatherApi 
 {
+
+  static MAX_FORECAST_DAYS = 8;
+
+  static DEFAULT_FORECAST_DAYS = 7;
+
     constructor(apiKey) {
-        this.endPoint = "https://api.openweathermap.org/data/2.5/onecall";
+        this.endPoint = OPEN_WEATHER_API_ONE_CALL;
         this.apiKey = apiKey;
     }
 
-    async getSevenDayForecast(lat, lng){
+    async getDailyForecast(lat, lng, numDays) {
     //let city = await this.getCoordinates(zipcode);
 
     // console.log("city " , city);
@@ -18,19 +40,14 @@ class OpenWeatherApi
       units: "imperial",
       appid: this.apiKey
     };
-    console.log(this.endPoint + "?" + formatQueryString(params));
+
+    numDays = numDays || DEFAULT_FORECAST_DAYS;
+    
     return fetch(this.endPoint + "?" + formatQueryString(params))
     .then(response => response.json())
     .then(forecast => {
-      console.warn("FORECAST: " , forecast);
-      let sevenDayForecast = forecast.daily;
-      // Sometimes the api returns an eight day forecast.
-      // Turn an eight day forecast into a seven day forecast.
-      sevenDayForecast.splice(7, 1);
-      this.sevenDayForecast = sevenDayForecast;
-      //let timeZoneOffset = forecast.timezone_offset;
-      // console.log("SDF " , sevenDayForecast);
-      return sevenDayForecast;
+      let daily = forecast.daily;
+      return daily.slice(0,DEFAULT_FORECAST_DAYS);
     });
   }
 }
