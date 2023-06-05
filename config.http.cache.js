5308552c6dd58f2631d18ca2d7861e80c758bcf2
Ubuntu <ubuntu@ip-172-31-58-171.us-west-2.compute.internal>
2023-06-04 17:38:34 -0700

    Provide example cache during development.

---
+++ config.http.cache.js
@@ -0,0 +1,24 @@
+
+
+export default cache;
+
+const cache = {
+    "api.google.com/calendar/work/event/list": JSON.stringify([
+        {
+            "id": "abcd-1234",
+            "status": "active",
+            "summary": "My event",
+            "description": "My description",
+            "start": {
+                "date": "2023-06-08",
+                "datetime": "2023-06-08Thh:mm:ss+00:00",
+                "timezone": null
+            },
+            "end": {
+                "date": null,
+                "datetime": null,
+                "timezone": null
+            }
+        }
+    ])
+};
